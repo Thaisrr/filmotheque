ada1f723de9b7aa64d7fed3559f8f509b7a08d0f
Aurelien Walter <awalter@dawan.fr>
2022-07-06 09:22:20 +0200

    :tada:

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyFilmotheque/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import MyFilmotheque from "@/components/MyFilmotheque";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyFilmotheque
   }
 }
 </script>
@@ -19,8 +18,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
 </style>
---
+++ src/components/MyFilmotheque.vue
@@ -0,0 +1,70 @@
+<template>
+  <main>
+  <h1>Filmothèque</h1>
+
+
+    <section class="grid">
+      <div class="card" v-for="(f) of films" :key="f.id">
+        <div class="c-header">
+          <img :src="f.img" :alt="f.title">
+        </div>
+        <div class="c-body">
+          <h3 :class="{seen: f.vu}">{{f.title}}</h3>
+          <button @click="setVue(f)">{{ f.vu? 'Marquer comme non vu'  : 'Marquer comme vu' }}</button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+name: "MyFilmotheque",
+data() {
+  return {
+    films: [
+      {id: 1, title: 'Retour vers le Futur 1', img : '', vu: true},
+      {id: 2, title: 'Retour vers le Futur 2', img : '', vu: true},
+      {id: 3, title: 'Retour vers le Futur 3', img : '', vu: true},
+      {id: 4, title: 'Jurrassic Parc', img : '', vu: true},
+      {id: 5, title: 'Police Academy 1', img : '', vu: false},
+      {id: 6, title: 'Police Academy 2', img : '', vu: false},
+    ]
+  }
+},
+  methods: {
+    setVue(film) {
+      film.vu = !film.vu;
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin-bottom: 50px;
+}
+.seen::before {
+  content: "✔️" ;
+}
+
+.grid {
+  display: flex;
+  gap: 30px;
+  flex-wrap: wrap;
+}
+
+.card {
+  width: min(100%, 300px);
+  box-shadow: 2px 2px 8px #a9a9a9;
+  padding: 20px;
+  border-radius: 5px;
+}
+
+main {
+  width: 80%;
+  margin: auto;
+}
+
+
+</style>
