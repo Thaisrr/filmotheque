3d890462371778505afbc273df34b43d8b6a8a0c
Aurelien Walter <awalter@dawan.fr>
2022-07-08 14:13:30 +0200

    :tada: Authentification et guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
-import MyFilmotheque from "@/views/MyFilmotheque";
 import Login from "@/views/Login";
 import isLogged from "@/guards/isLogged";
-import Details from "@/views/Details";
 
 const routes = [
     {
